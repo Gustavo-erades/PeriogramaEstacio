916ef4d0fb2abaa34ee46dc4d7bebc18d6365dd6
coddeqs <coddeqs@gmail.com>
2024-10-04 20:26:52 -0300

    Banco de dados do periograma

---
+++ database/Periograna_SQL.sql
@@ -0,0 +1,16 @@
+create database periograna_bd
+use periograna_bd
+
+default character set utf8
+default collate utf8_general_ci
+
+
+create table login_aluno (
+Email varchar (40), int not null,
+senha varchar (12) int not null,
+
+create table login_adm (
+Email varchar (40), int not null,
+senha varchar (12) int not null,
+
+ 
